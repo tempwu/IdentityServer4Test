d024b82e3b6709d1bc1748b03737387028155d99
tempwu02 <tempwu02@163.com>
2019-01-16 16:25:22 +0800

    add

--- IdentityServer4Test.IDP/Startup.cs
+++ IdentityServer4Test.IDP/Startup.cs
@@ -1,20 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using IdentityServer4Test.IDP.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace IdentityServer4Test.IDP
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public string BasePath { get; }
+
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            BasePath = PlatformServices.Default.Application.ApplicationBasePath;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            InMemoryConfiguration.Configuration = this.Configuration;
+
+            services.AddIdentityServer()
+                //.AddDeveloperSigningCredential()
+                .AddSigningCredential(new X509Certificate2(Path.Combine(BasePath,
+                        Configuration["Certificates:CerPath"]),
+                    Configuration["Certificates:Password"]))
+                .AddTestUsers(InMemoryConfiguration.GetUsers().ToList())
+                .AddInMemoryClients(InMemoryConfiguration.GetClients())
+                .AddInMemoryApiResources(InMemoryConfiguration.GetApiResources());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +51,7 @@ namespace IdentityServer4Test.IDP
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
